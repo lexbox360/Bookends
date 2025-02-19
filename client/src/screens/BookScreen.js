a3ad8c68b680da6ac52494129a3f1912d2797287
lexrayne <30938032+lexrayne@users.noreply.github.com>
2025-02-19 05:17:48 -0400

    Initial commit

---
+++ client/src/screens/BookScreen.js
@@ -0,0 +1,70 @@
+import { useParams, Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from "../components/Rating";
+import books from "../books";
+
+const BookScreen = () => {
+    const { _id:bookId } = useParams();
+    const book = books.find((p) => p._id === bookId);
+
+    return (
+        <>
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={5}>
+                    <Image src={book.cover} alt={book.title} fluid />
+                </Col>
+                <Col md={4}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{book.title}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={book.rating} text={`${book.numReviews} reviews`} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>{book.description}</ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col>
+                                        <strong>${book.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Status:
+                                    </Col>
+                                    <Col>
+                                        <strong>{book.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button
+                                    className='btn-block'
+                                    type='button'
+                                    disabled={book.countInStock === 0}
+                                >
+                                    Add to Cart
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    );
+};
+
+export default BookScreen;
