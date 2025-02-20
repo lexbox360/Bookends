fab4596e5ea6cb76d8d0c23e415e464020282570
lexrayne <30938032+lexrayne@users.noreply.github.com>
2025-02-19 23:23:37 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import books from './data/books.js';
+
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running.');
+});
+
+app.get('/api/books', (req, res) => {
+    res.send(books);
+});
+
+app.get('/api/books/:id', (req, res) => {
+    const book = books.find((p) => p._id === req.params.id);
+    res.send(book);
+});
+
+
+app.listen(port, () => console.log(`Server running on port ${port}.`));
