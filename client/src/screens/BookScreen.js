b4e50c2bd1ebde8ca660042d4081d24144dece76
lexrayne <30938032+lexrayne@users.noreply.github.com>
2025-02-22 00:22:59 -0400

    Replaced axios with booksApiSlice

--- client/src/screens/BookScreen.js
+++ client/src/screens/BookScreen.js
@@ -1,78 +1,76 @@
-import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-import axios from 'axios';
-import Rating from '../components/Rating';
+import { useParams, Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import { useGetBookDetailsQuery } from "../slices/booksApiSlice";
 
 const BookScreen = () => {
-    const [book, setBook] = useState({});
-    const { id: bookId } = useParams();
+  const { id: bookId } = useParams();
 
-    useEffect(() => {
-        const fetchBook = async () => {
-            const { data } = await axios.get('/api/books/${bookId}');
-            setBook(data);
-          }
-    }, [bookId]);
+  const { data: book, isLoading, error } = useGetBookDetailsQuery(bookId);
 
-    return (
-        <>
-            <Link className='btn btn-light my-3' to='/'>
-                Go Back
-            </Link>
-            <Row>
-                <Col md={5}>
-                    <Image src={book.cover} alt={book.title} fluid />
-                </Col>
-                <Col md={4}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h3>{book.title}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value={book.rating} text={`${book.numReviews} reviews`} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>{book.description}</ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        Price:
-                                    </Col>
-                                    <Col>
-                                        <strong>${book.price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        Status:
-                                    </Col>
-                                    <Col>
-                                        <strong>{book.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Button
-                                    className='btn-block'
-                                    type='button'
-                                    disabled={book.countInStock === 0}
-                                >
-                                    Add to Cart
-                                </Button>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
-        </>
-    );
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+
+      { isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? ( error?.data?.message || error.error ) : (
+      <>
+      <Row>
+        <Col md={5}>
+          <Image src={book.cover} alt={book.title} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{book.title}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={book.rating} text={`${book.numReviews} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>{book.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${book.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {book.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={book.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+      </>
+    )}
+    </>
+  );
 };
 
 export default BookScreen;
