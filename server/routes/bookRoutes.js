e21d2ec3bb3dde0546f75959bc4505352dbabb3c
lexrayne <30938032+lexrayne@users.noreply.github.com>
2025-02-21 23:11:45 -0400

    Added controller routes

--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,25 +1,8 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-import Book from "../models/bookModel.js";
+import { getBooks, getBookById } from "../controllers/bookController.js";
 
-router.get("/", asyncHandler(async (req, res) => {
-    const books = await Book.find({});
-    res.json(books);
-}));
-
-router.get("/:id", asyncHandler(async (req, res) => {
-    const book = await Book.findById(req.params.id);
-
-    if (book) {
-        return res.json(book);
-    } else {
-        res.status(404);
-        throw new Error('Resource not found -- No book with that ID.');
-    }
-
-    res.status(404).json({ message: "Book not found" })
-
-}));
+router.route('/').get(getBooks);
+router.route('/:id').get(getBookById);
 
 export default router;
