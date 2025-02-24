3f1503a2473ebcdf853516a0d65d927a03d2209f
lexrayne <30938032+lexrayne@users.noreply.github.com>
2025-02-24 03:45:38 -0400

    Added cart functionality

--- client/src/screens/BookScreen.js
+++ client/src/screens/BookScreen.js
@@ -1,14 +1,24 @@
-import { useParams, Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import Rating from "../components/Rating";
-import { useGetBookDetailsQuery } from "../slices/booksApiSlice";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
+import { useState } from 'react';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import { Form, Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useGetBookDetailsQuery } from '../slices/booksApiSlice';
+import { addToCart } from '../slices/cartSlice';
 
 const BookScreen = () => {
   const { id: bookId } = useParams();
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [qty, setQty] = useState(1);
   const { data: book, isLoading, error } = useGetBookDetailsQuery(bookId);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...book, qty }));
+    navigate('/cart');
+  }
 
   return (
     <>
@@ -60,11 +70,33 @@ const BookScreen = () => {
                   </Col>
                 </Row>
               </ListGroup.Item>
+
+              {book.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Quantity:</Col>
+                    <Col>
+                      <Form.Control
+                        as='select'
+                        value={qty}
+                        onChange={(e) => setQty(Number(e.target.value))}>
+                          {[...Array(book.countInStock).keys()].map((i) => (
+                            <option key={ i + 1 } value={ i + 1 }>
+                              { i + 1 }
+                            </option>
+                          ))}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
+
               <ListGroup.Item>
                 <Button
                   className="btn-block"
                   type="button"
                   disabled={book.countInStock === 0}
+                  onClick={addToCartHandler}
                 >
                   Add to Cart
                 </Button>
